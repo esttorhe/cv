7a3af41dd213676861bd9c546965ea6026566427
Esteban Torres <me@estebantorr.es>
2018-02-23 11:00:55 +0100

    Use correct name for encrypted key

--- upload.sh
+++ upload.sh
@@ -32,12 +32,12 @@ if cmp --silent $OLD $NEW ; then
   git add ./docs/*
   git commit -m "Deploy to «docs»: ${SHA}"
   
-  # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+  # Get the deploy key by using Travis's stored variables to decrypt travis_cv_id_rsa.enc
   ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
   ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
   ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
   ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../travis_cv_id_rsa.enc -out ../deploy_key -d
   chmod 600 ../deploy_key
   eval `ssh-agent -s`
   ssh-add deploy_key
